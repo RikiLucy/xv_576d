6c2391e7172999049fcff3dbff1bfc5de3042b55
RikiLucy <riki.and.lucy@gmail.com>
2016-08-31 23:02:18 +0700

    show posts

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.main')
-@section('articles')
+@section('content')
   @foreach ($post as $posts)
   <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
     <div class="mdl-card mdl-cell mdl-cell--12-col">
+
+      <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text">{{ $posts->title }}</h2>
+      </div>
+      <div class="mdl-card__subtitle-text">
+        &nbsp;&nbsp;{{ $posts->created_at}}
+        <br>
+        &nbsp;&nbsp;{{ $posts->author }}
+      </div>
       <div class="mdl-card__supporting-text">
-        <h4>{{ $posts->title }}</h4>
-        {{ $posts->text }}
+
+        {!!  $posts->text !!}
+
       </div>
       <div class="mdl-card__actions">
         <a href="#" class="mdl-button">Читать полностью</a>
